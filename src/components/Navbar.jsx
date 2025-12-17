a031abe442f9c3005a49b82098ddc882bd9038d4
Suraj Nair <surajnair9698@gmail.com>
2025-12-17 23:36:43 +0530

    feat: add navbar with demo links and styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+const navItems = [
+  { name: "Home", href: "#home" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+];
+function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.screenY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <nav
+      className={`fixed w-full z-40 transition-all duration-300  ${
+        isScrolled
+          ? "py-3 bg-background/80 backdrop-blur-md shadown-xs"
+          : "py-5"
+      }`}
+    >
+      <div className="container flex items-center justify-between">
+        <a href="" className="text-xl font-bold text-primary flex items-center">
+          <span className="relative z-10 ">
+            <span className="text-glow hover:text-glow text-foreground">
+              Suraj.dev {""}
+            </span>
+          </span>
+        </a>
+        {/*Desktop Version*/}
+        <div className="hidden md:flex space-x-8 backdrop-blur-xl">
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              className="text-foreground/80 hover:text-primary transition-colors duration-300"
+              href={item.href}
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </div>
+
+      {/*Mobile Version*/}
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <div className=" min-h-screen bg-background text-foreground">
       <ThemeToggle />
       <StarBackground />
+      <Navbar />
     </div>
   );
 }
